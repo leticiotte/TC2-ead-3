1dd82510155c55d158610ffad76466180645ec9d
Letícia Gonçalves <leticiaespregagon@gmail.com>
2022-05-30 01:09:05 -0300

    feat: inicializa projeto e front-end

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MessagesComponent } from './messages/messages.component';
 
-const routes: Routes = [];
 
+const routes: Routes = [
+  { path: "home", component: HomeComponent },
+  { path: "messages", component: MessagesComponent },
+  { path: "", redirectTo: "/home", pathMatch: "full" },
+  {path: '**', redirectTo: "/home", pathMatch: "full" }
+]
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { ToastrModule } from 'ngx-toastr';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    ToastrModule.forRoot(),
+    FormsModule
   ],
-  providers: [],
+  providers: [/*{
+    provide: HTTP_INTERCEPTORS,
+    useClass: LogInterceptor,
+    multi: true
+  }*/],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
